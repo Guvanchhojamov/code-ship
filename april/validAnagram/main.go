6ac1fac5f6e79d5cb34a803ca95b8bf692f2eee1
guvanchh <guvanchhojamov@gmail.com>
2024-05-02 17:28:57 +0500

    isAnagram2

--- april/validAnagram/main.go
+++ april/validAnagram/main.go
@@ -10,24 +10,24 @@ func main() {
 	t := "nagaram"
 	fmt.Println(isAnagram(s, t))
 }
+
 func isAnagram(s string, t string) bool {
 	if len(s) != len(t) {
 		return false
 	}
 
-	sChars := map[byte]int{}
-	tChars := map[byte]int{}
-	for i := 0; i < len(s); i++ {
-		sChars[s[i]]++
-		tChars[t[i]]++
-	}
+	var sSum, tSum uint64
+	var sM = uint64(1)
+	var tM = uint64(1)
 
-	for i, v := range sChars {
-		if v != tChars[i] {
-			return false
-		}
+	for i := 0; i < len(s); i++ {
+		sM *= uint64(s[i])
+		sSum += uint64(s[i])
+		tM *= uint64(t[i])
+		tSum += uint64(t[i])
+
 	}
-	return true
+	return sM+sSum == tM+tSum
 }
 
 //func isAnagram(s string, t string) bool {
@@ -35,16 +35,17 @@ func isAnagram(s string, t string) bool {
 //		return false
 //	}
 //
-//	var sSum, tSum uint64
-//	var sM = uint64(1)
-//	var tM = uint64(1)
-//
+//	sChars := map[byte]int{}
+//	tChars := map[byte]int{}
 //	for i := 0; i < len(s); i++ {
-//		sM *= uint64(s[i])
-//		sSum += uint64(s[i])
-//		tM *= uint64(t[i])
-//		tSum += uint64(t[i])
-//
+//		sChars[s[i]]++
+//		tChars[t[i]]++
 //	}
-//	return sM+sSum == tM+tSum
+//
+//	for i, v := range sChars {
+//		if v != tChars[i] {
+//			return false
+//		}
+//	}
+//	return true
 //}

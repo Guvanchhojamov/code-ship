fea40b8dc3398b8892bf8e445b4cee437bef3533
guvanchh <guvanchhojamov@gmail.com>
2024-05-02 17:24:53 +0500

    isAnagram

--- april/validAnagram/main.go
+++ april/validAnagram/main.go
@@ -10,22 +10,42 @@ func main() {
 	t := "nagaram"
 	fmt.Println(isAnagram(s, t))
 }
+
 func isAnagram(s string, t string) bool {
 	if len(s) != len(t) {
 		return false
 	}
 
-	sChars := map[byte]int{}
-	tChars := map[byte]int{}
+	var sSum uint64
+	var sM = uint64(1)
+	var tSum uint64
+	var tM = uint64(1)
 	for i := 0; i < len(s); i++ {
-		sChars[s[i]]++
-		tChars[t[i]]++
-	}
+		sM *= uint64(s[i])
+		sSum += uint64(s[i])
+		tM *= uint64(t[i])
+		tSum += uint64(t[i])
 
-	for i, v := range sChars {
-		if v != tChars[i] {
-			return false
-		}
 	}
-	return true
+	return sM+sSum == tSum+tM
 }
+
+//func isAnagram(s string, t string) bool {
+//	if len(s) != len(t) {
+//		return false
+//	}
+//
+//	sChars := map[byte]int{}
+//	tChars := map[byte]int{}
+//	for i := 0; i < len(s); i++ {
+//		sChars[s[i]]++
+//		tChars[t[i]]++
+//	}
+//
+//	for i, v := range sChars {
+//		if v != tChars[i] {
+//			return false
+//		}
+//	}
+//	return true
+//}

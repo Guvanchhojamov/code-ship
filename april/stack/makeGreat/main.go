c94fca042b55d68150aca21c284889e537867036
guvanchh <guvanchhojamov@gmail.com>
2024-05-08 12:41:23 +0500

    added md file

--- april/stack/makeGreat/main.go
+++ april/stack/makeGreat/main.go
@@ -7,18 +7,22 @@ import (
 const requiredDiff = 32
 
 func main() {
-	fmt.Println(makeItGreat("abBAcC"))
+	fmt.Println(makeGood("Pp"))
 }
 
-func makeItGreat(s string) string {
+func makeGood(s string) string {
 	var stack []byte
 	for i := 0; i < len(s); i++ {
 		stack = append(stack, s[i])
 		if len(stack) > 1 {
-			if stack[len(stack)-2]-stack[len(stack)-1] == requiredDiff {
-				stack = stack[0 : len(stack)-2]
-			}
+			stack = checkAndPop(stack)
 		}
 	}
 	return string(stack)
+}
+func checkAndPop(stack []byte) []byte {
+	if stack[len(stack)-2]-stack[len(stack)-1] == requiredDiff || stack[len(stack)-1]-stack[len(stack)-2] == requiredDiff {
+		stack = stack[0 : len(stack)-2]
+	}
+	return stack
 }

8ec7d7b265729f3b0cd0a85432312b82d1beb201
guvanchh <guvanchhojamov@gmail.com>
2024-05-30 18:41:26 +0500

    fcm ready

--- easy/Hash/ ContainsDuplicate/main.go
+++ easy/Hash/ ContainsDuplicate/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 /*
-*
+
 Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
 Example 1:
@@ -14,8 +14,8 @@ Example 2:
 
 Input: nums = [1,2,3,4]
 Output: false
-Example 3:
 
+Example 3:
 Input: nums = [1,1,1,3,3,4,3,2,4,2]
 Output: true
 */

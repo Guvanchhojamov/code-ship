c2f3ce3fe7099b567f0b91d2427cecde184c85c7
guvanchh <guvanchhojamov@gmail.com>
2024-06-14 08:52:05 +0500

    merge sorted array

--- twoPointers/mergeArray/main.go
+++ twoPointers/mergeArray/main.go
@@ -3,25 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	mergeSortedArray([]int{1, 2, 3, 0, 0, 0}, []int{2, 5, 6}, 3, 3)
+	mergeSortedArray([]int{2, 5, 6, 0, 0, 0}, []int{1, 2, 3}, 3, 3)
 }
 
 func mergeSortedArray(nums1, nums2 []int, m, n int) {
-	m, n, last := m-1, n-1, m+n-1
+	i, j, last := m-1, n-1, m+n-1
 	//fmt.Println(nums1[m], nums2[n], nums1[k])
-	for m+n > 0 {
-		switch {
-		case nums1[m] > nums2[n]:
-			nums1[last] = nums1[m]
-			m--
-		case nums1[m] < nums2[n]:
-			nums1[last] = nums2[n]
-			n--
-		case nums1[m] == nums2[n]:
-			nums1[last] = nums2[n]
-			n--
+	for i >= 0 && j >= 0 {
+		if nums1[i] > nums2[j] {
+			nums1[last] = nums1[i]
+			i--
+		} else {
+			nums1[last] = nums2[j]
+			j--
 		}
 		last--
 	}
-	fmt.Println(nums1)
+	fmt.Println(nums1, i, j, last)
+	for j >= 0 {
+		nums1[last] = nums2[j]
+		j--
+		last--
+	}
+	fmt.Println(nums1, i, j, last)
 }

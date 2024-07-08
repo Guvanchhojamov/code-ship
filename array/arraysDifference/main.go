04a596e5a60f6ef9c1407a40b9a39b9761fa271a
guvanchh <guvanchhojamov@gmail.com>
2024-07-09 00:13:10 +0500

    arrays difference

--- array/arraysDifference/main.go
+++ array/arraysDifference/main.go
@@ -3,11 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	twoArraysDifference([]int{1, 2, 3}, []int{2, 4, 6})
+	fmt.Println(twoArraysDifference([]int{1, 2, 3, 3}, []int{1, 1, 2, 2}))
 }
 
 func twoArraysDifference(nums1, nums2 []int) [][]int {
 	nums1Map, nums2Map := map[int]bool{}, map[int]bool{}
+	answer := make([][]int, 2)
 	for _, v := range nums1 {
 		nums1Map[v] = true
 	}
@@ -15,9 +16,17 @@ func twoArraysDifference(nums1, nums2 []int) [][]int {
 	for _, v := range nums2 {
 		nums2Map[v] = true
 	}
-	
-	for i
-	
-	fmt.Println(nums1Map, nums2Map)
-	return [][]int{}
+
+	for key := range nums1Map {
+		if _, ok := nums2Map[key]; !ok {
+			answer[0] = append(answer[0], key)
+		}
+	}
+
+	for key := range nums2Map {
+		if _, ok := nums1Map[key]; !ok {
+			answer[1] = append(answer[1], key)
+		}
+	}
+	return answer
 }

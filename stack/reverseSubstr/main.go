8c6eb60a3ff2f8b1b41a9ffad338018ec21c0da1
guvanchh <guvanchhojamov@gmail.com>
2024-07-17 20:50:37 +0500

    reverse

---
+++ stack/reverseSubstr/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	reverseSubstr("(oc)el")
+}
+
+/*
+(u(love)i)
+
+"(ed(et(oc))el)" => leetcode
+
+stack = (ed(et(oc
+tmpStr
+case v==')':
+
+	for v!='(':
+	  tmpStr = stack.pop()
+	if v=='(':
+	   stack.pop()
+	   stack.append(tmpStr)
+	   tmpStr=''
+
+default stack=append(v)
+*/
+func reverseSubstr(s string) string {
+	stack, tmpStack := []rune{}, []rune{}
+	for _, v := range s {
+		switch {
+		case v == ')':
+			fmt.Println(string(stack), len(stack))
+			for stack[len(stack)-1] != '(' && len(stack) > 0 {
+				tmpStack = append(tmpStack, stack[len(stack)-1])
+				stack = stack[:len(stack)-1]
+			}
+			if stack[len(stack)-1] == '(' {
+				stack = stack[:len(stack)-1]
+			}
+			stack = append(stack, tmpStack...)
+			fmt.Println(string(stack), string(tmpStack))
+			tmpStack = []rune{}
+		default:
+			stack = append(stack, v)
+		}
+	}
+	fmt.Println(string(stack))
+	return ""
+}

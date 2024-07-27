7d5104492c1f44de7fb4088d94c2987dd1a34fee
guvanchh <guvanchhojamov@gmail.com>
2024-07-27 17:44:09 +0500

    added missing number

--- otherTasks/sort/missingNum.py
+++ otherTasks/sort/missingNum.py
@@ -7,12 +7,15 @@ def missingNumber(nums: List[int]) -> int:
     while i < len(nums):
         if nums[i]<len(nums) and nums[i]!= nums[nums[i]]: 
             nums[nums[i]],nums[i]=nums[i],nums[nums[i]]
-        i+=1
+        else:
+            i+=1
 
+    print(nums)
     # Return number as answer if number is not in right index.
     for i in range(len(nums)):
+        print("i",i, "nums[i]",nums[i])
         if i != nums[i]:
-            return i 
+            return i
     return len(nums)
 
-print("missing number is: ", missingNumber([3,0,1]))
+print("missing number is: ", missingNumber([9,6,4,2,3,5,7,0,1]))

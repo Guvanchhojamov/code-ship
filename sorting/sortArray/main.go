3c85f84f369eecab4c61d0c4448cb200b4413a46
guvanchh <guvanchhojamov@gmail.com>
2024-07-30 00:29:41 +0500

    conting sort algo Part 1

--- sorting/sortArray/main.go
+++ sorting/sortArray/main.go
@@ -1,11 +1,38 @@
 package main
 
 func main() {
-
+	sortArray([]int{-5, 2, 3, 1})
 }
 
 // leetcode 912.
 func sortArray(nums []int) []int {
+	result := []int{}
+	minimum, maximum := findMinMax(nums)
+	var numCounts = make([]int, maximum+1-minimum)
 
-	return nil
+	for _, v := range nums {
+		numCounts[v-minimum]++
+	}
+
+	for i, val := range numCounts {
+		for val > 0 {
+			result = append(result, i+minimum)
+			val--
+		}
+	}
+	return result
+}
+
+func findMinMax(nums []int) (int, int) {
+	maximum := 0
+	minimum := nums[0]
+	for _, v := range nums {
+		if v > maximum {
+			maximum = v
+		}
+		if v < minimum {
+			minimum = v
+		}
+	}
+	return minimum, maximum
 }

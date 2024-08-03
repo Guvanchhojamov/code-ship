4882af926b2f1f7370757b52e78f635eb01dc158
guvanchh <guvanchhojamov@gmail.com>
2024-08-03 22:59:19 +0500

    valid matrix sum

---
+++ array/Greedy/validMatrix/main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	rowSum := []int{3, 8}
+	colSum := []int{4, 7}
+	restoreMatrix(rowSum, colSum)
+}
+
+func restoreMatrix(rowSum []int, colSum []int) [][]int {
+
+	return nil
+}

b279a3bb80d4d6e81235e3e7a562832930c4702f
guvanchh <guvanchhojamov@gmail.com>
2024-08-10 23:58:00 +0500

    recursive buble sort

--- otherTasks/sort/bubleSort/main.go
+++ otherTasks/sort/bubleSort/main.go
@@ -4,6 +4,8 @@ import "fmt"
 
 func main() {
 	sortNums([]int{5, 4, 3, 2, 1})
+	recSort := recursiveSort([]int{5, 4, 3, 2, 1}, 0)
+	fmt.Println("recSort", recSort)
 }
 
 func sortNums(nums []int) {
@@ -16,4 +18,19 @@ func sortNums(nums []int) {
 		}
 	}
 	fmt.Println(nums)
+}
+
+func recursiveSort(nums []int, i int) []int {
+	if i >= len(nums) {
+		return nums
+	}
+	j := i + 1
+	for j < len(nums) {
+		if nums[i] > nums[j] {
+			nums[i], nums[j] = nums[j], nums[i]
+		}
+		j++
+	}
+	i++
+	return recursiveSort(nums, i)
 }

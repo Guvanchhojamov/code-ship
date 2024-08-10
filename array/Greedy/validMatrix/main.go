5948880983527413f43d45309b93a0e9171f2a92
guvanchh <guvanchhojamov@gmail.com>
2024-08-10 11:44:48 +0500

    valid matrix

--- array/Greedy/validMatrix/main.go
+++ array/Greedy/validMatrix/main.go
@@ -6,6 +6,30 @@ func main() {
 	restoreMatrix(rowSum, colSum)
 }
 
+/*
+
+Example 2:
+
+Input: rowSum = [5,7,10], colSum = [8,6,8]
+Output: [[0,5,0],
+         [6,1,0],
+         [2,0,8]]
+*/
+
+/*
+    8 6 8
+  5 0 0 0
+  7 0 0 0
+ 10 0 0 0
+
+
+ 1. set min(rows[i], cols[j]) for col 0
+    8 6 8
+  5 5 0 0
+  7 3 0 0
+10  0 0 0
+*/
+
 func restoreMatrix(rowSum []int, colSum []int) [][]int {
 
 	return nil

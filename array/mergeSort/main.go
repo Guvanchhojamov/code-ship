f7aa7c15d6b9d8af0208f68a092d491309f3d2de
guvanchh <guvanchhojamov@gmail.com>
2024-08-10 23:26:16 +0500

    merge sort complited

--- array/mergeSort/main.go
+++ array/mergeSort/main.go
@@ -1,54 +1,63 @@
-package main 
+package main
 
 import "fmt"
-import "bufio"
-import "os"
-import "strings"
 
-func main(){
-   // nums := []int{5,4,3,2,1}
-   // mergeSort(nums, 0, len(nums)-1)
-   readerTest()
+func main() {
+	nums := []int{5, 4, 3, 2, 1}
+	sorted := mergeSort(nums)
+	fmt.Println(sorted)
+	//readerTest()
 }
 
 /*
   n=5     l=0  mid =n/2  r=len-1
-  
-  1. 5 4 3    2 1 
+
+  1. 5 4 3    2 1
     n=3 l=0  mid=n/2  r=len-1
-    5 4        3 
+    5 4        3
     n=2 l=0   mid=n/2  r=len-1
-    5          4 
-    
-  2. 
+    5          4
+
+  2.
 */
 
-func mergeSort(nums []int,l, r int) []int{
-    mid := len(nums)-1 / 2 
-    //right :=  len(nums)-1 
-    if l >= r {
-      return nums
-    }
+func mergeSort(nums []int) []int {
+	mid := len(nums) / 2
+	//right :=  len(nums)-1
+	if len(nums) < 2 {
+		return nums
+	}
 
-    mergeSort(nums[l:mid],l,mid)
-    fmt.Println("nums1",nums)
-    //nums =  mergeSort(nums,mid+1, right)
-    //fmt.Println("nums2",nums)
-    return nums
+	//fmt.Println(nums[l:mid])
+	//fmt.Println(nums[mid:r])
+
+	leftSlice := mergeSort(nums[:mid])
+	fmt.Println("leftSlice", leftSlice)
+	rightSlice := mergeSort(nums[mid:])
+	fmt.Println("rightSlice", rightSlice)
+	return merge(leftSlice, rightSlice)
 }
 
+func merge(leftSlice, rightSlice []int) []int {
+	i, j := 0, 0
+	mergedSlice := []int{}
+	for i < len(leftSlice) && j < len(rightSlice) {
+		if leftSlice[i] < rightSlice[j] {
+			mergedSlice = append(mergedSlice, leftSlice[i])
+			i++
+		} else {
+			mergedSlice = append(mergedSlice, rightSlice[j])
+			j++
+		}
+	}
 
-// func readerTest(){
-//       reader := bufio.NewReader(os.Stdin)
-//        fmt.Println("Running...")
-
-//       // Проверяем, была ли нажата клавиша Enter
-//       input, _ := reader.ReadString('\n')
-
-//       input = strings.TrimSpace(input)
-//       sum:= 0 
-//       numsStr := strings.Split(input, " ")
-        
-//       fmt.Println("input", input, sum)
-// }
-
+	for i < len(leftSlice) {
+		mergedSlice = append(mergedSlice, leftSlice[i])
+		i++
+	}
+	for j < len(rightSlice) {
+		mergedSlice = append(mergedSlice, rightSlice[j])
+		j++
+	}
+	return mergedSlice
+}

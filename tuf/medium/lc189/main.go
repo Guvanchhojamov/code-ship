b8d2af677a0be1c636764fc7e469f29d085b4f6c
guvanchh <guvanchhojamov@gmail.com>
2024-08-24 17:07:41 +0500

    rotate array by k

--- tuf/medium/lc189/main.go
+++ tuf/medium/lc189/main.go
@@ -25,3 +25,45 @@ func rotate(nums []int, k int) {
 	}
 	//fmt.Println(k)
 }
+
+// this solution time limit exeptions on big test case. 
+// need optimization 
+// 
+/*
+	1 2 3 4 5 6 7   k=3    output: 5,6,7,1,2,3,4
+    1 2 3 4 5 6 7 0 0 0 0 
+	1 2 3 4 5 6 7 1 2 3 4 
+	        5 6 7 1 2 3 4 
+
+
+     2) [-1,-100,3,99], k = 2 
+	     [3,99,-1,-100]
+
+		 -1 -100 3 99 0 0 
+		 -1 -100 3 99 -1 -100
+		         3 99 -1 -100 
+		
+	
+*/
+
+// func rotate(nums []int, k int) {
+// 	if k > 0 {
+// 		for i := 0; i < len(nums); i++ {
+// 			nums[i], nums[len(nums)-1] = nums[len(nums)-1], nums[i]
+// 		}
+// 		rotate(nusm, k--)
+// 	}
+// 	return 
+// }
+
+
+
+func rotate(nums []int, k int) {
+	n := len(nums)
+	k = k % n
+	res := make([]int, n)
+	for i := 0; i < n; i++ {
+		res[(i+k)%n] = nums[i]
+	}
+	copy(nums, res)
+}

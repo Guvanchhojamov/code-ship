211c02c3261a76cb52f3296da860db72367659f3
guvanchh <guvanchhojamov@gmail.com>
2024-08-29 17:19:18 +0500

    single explanation

--- tuf/easy/lc113/singleNumber.py
+++ tuf/easy/lc113/singleNumber.py
@@ -18,9 +18,18 @@
 # Output: 1
 
 # Hint : XOR of two equal numbers gives 0 :)
+# 6^2=4 how? 
+#    1  1  0  (binary for 6)
+# ^  0  1  0  (binary for 2)
+# -------------
+#    1  0  0  (result in binary)
+
 def singleNumber(nums) -> int:
     res = 0
     for num in nums:
+        print("res", res)
+        print("num",num)
+        print("res^num", res^num)
         res ^= num    
     return res
 

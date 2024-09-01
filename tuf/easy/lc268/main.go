ce2d33cfdfb1f82e34140946a89cbeb69ece6f31
guvanchh <guvanchhojamov@gmail.com>
2024-09-01 21:42:52 +0500

    missing number with xor

--- tuf/easy/lc268/main.go
+++ tuf/easy/lc268/main.go
@@ -1,29 +1,50 @@
-package main 
+package main
 
 import "fmt"
-func  main(){
-    missingNumber([]int{3,0,1})
+
+func main() {
+	missingNumber([]int{3, 0, 1})
 }
 
 /*
-  3 0 1 
-  0 1 2 
+3 0 1
+0 1 2
+
+3 0 2
+0 1 2
+
+			if n[i] != i
+			   if n[i] == len(n)
+		            swap(n[i],n[len(n)-1])
+		       else:
+		       swap(n[n[i]], n[i])
+	        else:
+	        i++
+
+1 0 2
+0 1 2
+*/
+
+/*
+Solution with XOR logic.
+
+	0^a = a
+	a^a =  0
+	a^b = binary(a^b)
 */
 func missingNumber(nums []int) int {
-    i := 0;
-    j := 0;
-    for  j < len(nums) {
-        if nums[i] != i {
-            if nums[i] >= len(nums) {
-              nums[len(nums)-1], nums[i] = nums[i], nums[len(nums)-1]
-            }
-            fmt.Println(nums, nums[i], i )
-            nums[nums[i]],nums[i] = nums[i], nums[nums[i]]
-        }else{
-            i++
-        }
-        j++
-    }
-    fmt.Println(nums)
-    return 0
+	res := 0
+	actual := 0
+	for i := 0; i < len(nums); i++ {
+		actual = actual ^ i
+	}
+
+	for i := 0; i < len(nums); i++ {
+		res ^= nums[i]
+	}
+
+	actual = actual ^ len(nums)
+	fmt.Println(actual, res, res^actual)
+
+	return actual ^ res
 }
---
+++ tuf/easy/lc268/missingNum.py
@@ -0,0 +1,4 @@
+def missingNumber(nums):
+    print('i miss you', nums)
+
+missingNumber([1,2,3,4,5])

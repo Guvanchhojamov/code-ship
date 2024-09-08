c292a13cf154df5ed3374d1dbcc15813fa7bba74
guvanchh <guvanchhojamov@gmail.com>
2024-09-08 16:08:15 +0500

    refixsum range

--- tuf/medium/prefixSum/main.go
+++ tuf/medium/prefixSum/main.go
@@ -3,14 +3,27 @@ package main
 import "fmt"
 
 func main(){
-    prefixSums([]int{1,2,3,4,5})
+    prefixSums([]int{1,2,3,4,5},2,3)
 }
 
-func prefixSums(nums []int ){
+func prefixSums(nums []int, left, right int ){
     var pSum = make([]int, 0, len(nums))
+    var subSum = 0
     pSum = append(pSum,nums[0])
     for i:=1; i<len(nums); i++{
         pSum = append(pSum,pSum[i-1]+nums[i])
     }
-    fmt.Println(pSum)
+
+    if left<0 || right>=len(nums) {
+        fmt.Println("invalid left or right")
+        return 
+    }
+
+    if left == 0{
+        subSum = pSum[right]
+    }else if right<len(nums){
+        subSum=pSum[right]-pSum[left-1]
+    }
+
+    fmt.Println(pSum, subSum)
 }   

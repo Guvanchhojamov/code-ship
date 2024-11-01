ac58f81dbef97236166c2878df2e97e0386b0ffa
guvanchh <guvanchhojamov@gmail.com>
2024-11-01 08:44:31 +0500

    prefix sums

--- tuf/medium/prefixSum/main.go
+++ tuf/medium/prefixSum/main.go
@@ -2,60 +2,71 @@ package main
 
 import "fmt"
 
-func main(){
-//    prefixSums([]int{1,2,3,4,5},2,3)
-    subArraySumK([]int{3, 4, 7, 2, -3, 1, 4, 2}, 7)
+func main() {
+	//    prefixSums([]int{1,2,3,4,5},2,3)
+	subArraySumK([]int{1, 2, 1, 2, 1}, 3)
 }
 
-func prefixSums(nums []int, left, right int ){
-    var pSum = make([]int, 0, len(nums))
-    var subSum = 0
-    pSum = append(pSum,nums[0])
-    for i:=1; i<len(nums); i++{
-        pSum = append(pSum,pSum[i-1]+nums[i])
-    }
+func prefixSums(nums []int, left, right int) {
+	var pSum = make([]int, 0, len(nums))
+	var subSum = 0
+	pSum = append(pSum, nums[0])
+	for i := 1; i < len(nums); i++ {
+		pSum = append(pSum, pSum[i-1]+nums[i])
+	}
 
-    if left<0 || right>=len(nums) {
-        fmt.Println("invalid left or right")
-        return 
-    }
+	if left < 0 || right >= len(nums) {
+		fmt.Println("invalid left or right")
+		return
+	}
 
-    if left == 0{
-        subSum = pSum[right]
-    }else if right<len(nums){
-        subSum=pSum[right]-pSum[left-1]
-    }
+	if left == 0 {
+		subSum = pSum[right]
+	} else if right < len(nums) {
+		subSum = pSum[right] - pSum[left-1]
+	}
 
-    fmt.Println(pSum, subSum)
-}   
+	fmt.Println(pSum, subSum)
+}
 
 /*
-  1 2 3 4 5  k=5 , out=2 
-  1 3 6 10 15 
-  0:1 
+  1 2 3 4 5  k=5 , out=2
+  1 3 6 10 15
+  0:1
   currSum +=nums[i]
 
 
 
 */
 
-
 /*
-  nums = 3 4 7 2 -3 1 4 2    k =  7    out=4; 
- 
-  sum[j]-sum[i] = k  
+  nums = 3 4 7 2 -3 1 4 2    k =  7    out=4;
+
+  sum[j]-sum[i] = k
   sum[j]-k = sum[i]
 
+  0 1+1   1
+  7 1+1
+  11 1+1   3
+  13 1 + 1  4
+          2
+
+
+1, 2, 1, 2, 1
 */
 
-func subArraySumK(nums []int, k int) int{
-    pSums := map[int]int{}
-    var currSum = 0
-    for i:=0;i<len(nums);i++{
-        currSum = currSum + nums[i]
-        pSums[i] = currSum
-    }
+func subArraySumK(nums []int, k int) int {
+	pSusm := map[int]int{}
+	count, diff, currSum := 0, 0, 0
+	pSusm[0] = 1
+	for i := 0; i < len(nums); i++ {
+		currSum += nums[i]
+		diff = currSum - k
+		if val, ok := pSusm[diff]; ok {
+			count += val
+		}
+		pSusm[currSum]++
 
-    fmt.Println(pSums, currSum)
-    return 0 
+	}
+	return count
 }

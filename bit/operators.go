3d2c9d755e9cdd8b62e0306ac74d8daea83b2c41
guvanchh <hojamow.guwanch@gmail.com>
2025-05-14 17:34:37 +0500

    bit manipulations

---
+++ bit/operators.go
@@ -0,0 +1,13 @@
+package bit
+
+/*
+ For bit manipulation we have some operators,
+ and,or,xor,not,shift left, shift right.
+ and = &&  both or not.
+ or  = || 1 of both
+ xor = must 1 of both.
+ not = ! in reverse manner.
+ shift we have 2 shifts left and right.
+ shift means 1 bit left or right.
+
+*/

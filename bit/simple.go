80ffd1a9f346cea6f5a0f9d872d88e139b0b5feb
guvanchh <hojamow.guwanch@gmail.com>
2025-05-16 13:02:34 +0500

    another daily dp, not solved

---
+++ bit/simple.go
@@ -0,0 +1,13 @@
+package bit
+
+/*
+We try to solve simple problems, related with bit manipulations.
+1. Swap two numbers
+2. Check if ith  bit is set or not
+3. set ith Bit
+4. clear ith Bit
+5. Toggle ith Bit
+6. Remove the last set Bit
+7. Check if a number is power of 2 or not
+8. Count the number of set bits
+*/

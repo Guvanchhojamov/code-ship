a416c8ffd6fcc2e675cf919011daf47407975b3e
guvanchh <hojamow.guwanch@gmail.com>
2025-06-23 18:09:15 +0500

    sliding window problems intro

---
+++ sliding-window/intro.go
@@ -0,0 +1,28 @@
+package sliding_window
+
+/*
+Intro for sliding window and two pointer playlist. from tuf.
+There few patterns for where we can use sliding window or two pointer patterns:
+1. Constant window.
+2. Longest subarray/substring (window size) with <condition>
+3. Number of subarrays where condition <condition>
+	- solution comes using pattern-2, with separating condition 2 or more parts.
+4.Shortest/Minimum window <condition>
+- pattern-1 used in more problems. Simple keep correct window problems.
+- p-2 used median number of problems.
+- in 3 also we use p1 or p2 with separating condition to parts.
+- p-4 is more less problems with this related tasks.
+*/
+/*
+ Let's solve simple example questions for each pattern.
+p1+p2:
+-Constant window: Print all windows with len k
+-Largest substring/subarray (size) with sum<=k.
+p3:
+- Number of subarrays where sum=k
+p4:
+- Shortest/Minimum subarray with sum >= k
+*/
+/*
+
+ */

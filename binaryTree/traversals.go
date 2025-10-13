d56fc40ce2871b91ccb2bf8e38005a0e7d2fc3f9
guvanchh <guvanchhojamov@gmail.com>
2025-10-13 14:35:12 +0500

    btree introduction

---
+++ binaryTree/traversals.go
@@ -0,0 +1,14 @@
+package binarytree
+
+/*
+ There are 2 algos of treaversal:
+1. DFS; 2.BFS.
+DFS:  uses recursive algo with stack ds.
+	- inorder traversal: left,right,root.
+	- preorder traversal: root, left,right
+	- postorder traversal: left,root,right.
+
+BFS: uses queue ds with recursive algo.
+	- Level order traversal: Visit nodes, of level0,1,2, ... visit all nodes level by level.
+
+*/
